6799bce2011e2b58e222bf7c84863cb85da8b95e
Daniel Kinuthia <danshotit91@gmail.com>
2025-09-25 14:04:44 +0300

    second hand browse

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -158,7 +158,7 @@ const allCars: CarProps[] = [
   {
     id: 22,
     title: "2014 BMW 116i",
-    image: "",
+    image: "/lovable-uploads/r.jpg",
     price: "KES 1,500,000",
     year: 2014,
     mileage: "",
@@ -170,7 +170,7 @@ const allCars: CarProps[] = [
   {
     id: 23,
     title: "2011 Volkswagen Touareg TDI",
-    image: "",
+    image: "/lovable-uploads/s.jpg",
     price: "KES 2,750,000",
     year: 2011,
     mileage: "",
