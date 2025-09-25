c705f5ac3b840cef96e4eaf46946bfdd49115ff0
Daniel Kinuthia <danshotit91@gmail.com>
2025-09-25 05:34:20 +0300

    waigera

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
             <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
               <Car className="w-5 h-5 text-primary-foreground" />
             </div>
-            <span className="text-xl font-bold text-foreground">Waigera</span>
+            <span className="text-xl font-bold text-foreground"></span>
           </Link>
 
           {/* Desktop Navigation */}
